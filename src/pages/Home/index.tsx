79c8155fad28dec86eb9a1ccc8d6d26c6c5259a4
Brian <brianmazini@gmail.com>
2024-08-29 08:54:47 -0300

    formatação da data e dos status do histórico

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,10 +15,10 @@ import { useContext } from 'react'
 import { CyclesContext } from '../../contexts/CyclesContext'
 
 const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
+  task: zod.string().min(5, 'Informe a tarefa'),
   minutesAmount: zod
     .number()
-    .min(1, 'O ciclo precisa ser no mínimo de 5 minutos.')
+    .min(5, 'O ciclo precisa ser no mínimo de 5 minutos.')
     .max(60, 'O ciclo precisa ser no máximo de 60 minutos.'),
 })
 
