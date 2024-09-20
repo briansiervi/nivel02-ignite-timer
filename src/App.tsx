33a0cbef373084c4cbf67368ba94b00345a4a3a0
briansiervi <brianmazini@gmail.com>
2024-09-20 08:25:53 -0300

    atualização do styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, StyleSheetManager } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './components/Router'
 import { BrowserRouter } from 'react-router-dom'
 import { CyclesContextProvider } from './contexts/CyclesContext'
+import isPropValid from '@emotion/is-prop-valid'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
         <CyclesContextProvider>
-          <Router />
+          <StyleSheetManager shouldForwardProp={isPropValid}>
+            <Router />
+          </StyleSheetManager>
         </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
